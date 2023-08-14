e6cb65acafe440dbfa0bf868936cf26494c28962
MauroDuque <mauricio.duque.eje@gmail.com>
2023-08-14 16:34:32 -0400

    V1.3

--- src/wifimanager.cpp
+++ src/wifimanager.cpp
@@ -7,7 +7,41 @@ const char* stringmac = macaddress.c_str();
 const char *ssid_mac = stringmac;
 char* soft_ap_password = NULL;
 
-char soft_ap_ssid[30];
+char soft_ap_ssid[30]
+int bars_rssi = 0;
+int rssi = 0;
+
+int get_rssi_strength() {
+    return rssi;
+}
+
+int get_rssi_strength_bars() {
+    return bars_rssi;
+}
+
+int rssiToBars(int rssi) {
+    if (rssi >= -50) {
+        return 4;
+    } else if (rssi >= -60) {
+        return 3;
+    } else if (rssi >= -70) {
+        return 2;
+    } else if (rssi >= -80) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void rssi_loop() {
+    rssi = WiFi.RSSI();  // Get RSSI value in dBm
+    bars_rssi = rssiToBars(rssi);
+
+    Serial.print("RSSI: ");
+    Serial.print(rssi);
+    Serial.print(" dBm, Bars: ");
+    Serial.println(bars_rssi);
+}
 
 String get_mac() {
     return macaddress;
--- src/wifimanager.h
+++ src/wifimanager.h
@@ -1,6 +1,10 @@
 #ifndef WIFIMANAGER_H
 #define WIFIMANAGER_H
 
+void rssi_loop();
+int get_rssi_strength_bars();
+int get_rssi_strength();
+
 void wifi_connection(const char* ssid, const char* password);
 bool is_internet();
 String get_mac();
