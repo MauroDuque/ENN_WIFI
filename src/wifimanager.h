15237ca6a822caf8f35be4a763cdef4f138af46b
MauroDuque <mauricio.duque.eje@gmail.com>
2023-08-15 18:46:38 -0400

    Wifi modes 7

--- src/wifimanager.h
+++ src/wifimanager.h
@@ -9,5 +9,5 @@ void wifi_connection(const char* ssid, const char* password);
 bool is_internet();
 int get_rssi_strength_bars();
 void setWiFiSleep(bool enableSleep);
-// void setWiFiMode(int modeSelection);
+
 #endif // WIFI_MANAGER_H
