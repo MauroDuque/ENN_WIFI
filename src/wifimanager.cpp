45335e2a4797c3a2450cc6065a6016d91a1cfbbd
MauroDuque <mauricio.duque.eje@gmail.com>
2023-08-15 18:31:15 -0400

    Wifi modes

--- src/wifimanager.cpp
+++ src/wifimanager.cpp
@@ -7,8 +7,18 @@ String get_mac_address() {
     return WiFi.macAddress();
 }
 
+// Function to change WiFi mode
+void changeWiFiMode(wifi_mode_t mode) {
+    WiFi.mode(mode);
+}
+
+// Function to control WiFi sleep mode
+void setWiFiSleep(bool enableSleep) {
+    WiFi.setSleep(enableSleep);
+}
+
 void wifi_connection(const char* ssid, const char* password) {
-    WiFi.mode(WIFI_AP_STA);
+    changeWiFiMode.mode(WIFI_AP_STA);
 
     String soft_ap_ssid = String(ssid_default) + WiFi.macAddress();
     WiFi.softAP(soft_ap_ssid.c_str(), soft_ap_password);
@@ -42,61 +52,3 @@ int get_rssi_strength_bars() {
         return 0;
     }
 }
-
-
-// #include "WiFi.h"
-
-// const char *ssid_default = "Enerinno";
-
-// String macaddress = WiFi.macAddress();
-// const char* stringmac = macaddress.c_str();
-// const char *ssid_mac = stringmac;
-// char* soft_ap_password = NULL;
-
-// char soft_ap_ssid[30]
-
-// String get_mac() {
-//     return macaddress;
-// }
-
-// void wifi_connection(const char* ssid, const char* password) {
-//     char soft_ap_ssid[64]; // Adjust the array size as needed
-//     strcpy(soft_ap_ssid, ssid_default);
-//     strcat(soft_ap_ssid, ssid_mac);
-
-//     Serial.println("Setting AP (Access Point & Station)");
-//     WiFi.mode(WIFI_AP_STA);
-
-//     WiFi.softAP(soft_ap_ssid, soft_ap_password);
-//     Serial.print("Access Point IP address: ");
-//     Serial.println(WiFi.softAPIP()); // Print the IP address of the Access Point
-
-//     WiFi.begin(ssid, password);
-
-//     Serial.println("NetWork: ");
-//     Serial.println(soft_ap_ssid);
-// }
-
-// bool is_internet() {
-//     if (WiFi.status() != WL_CONNECTED) {
-//         return false;
-//     }
-//     Serial.print("IP address: ");
-//     Serial.println(WiFi.localIP());
-
-//     return true;
-// }
-
-// int get_rssi_strength_bars() {
-//      if (WiFi.RSSI() >= -50) {
-//         return 4;
-//     } else if (WiFi.RSSI() >= -60) {
-//         return 3;
-//     } else if (WiFi.RSSI() >= -70) {
-//         return 2;
-//     } else if (WiFi.RSSI() >= -80) {
-//         return 1;
-//     } else {
-//         return 0;
-//     }
-// }
--- src/wifimanager.h
+++ src/wifimanager.h
@@ -7,5 +7,6 @@ String get_mac_address();
 void wifi_connection(const char* ssid, const char* password);
 bool is_internet();
 int get_rssi_strength_bars();
-
+void setWiFiSleep(bool enableSleep);
+void changeWiFiMode(wifi_mode_t mode);
 #endif // WIFI_MANAGER_H
